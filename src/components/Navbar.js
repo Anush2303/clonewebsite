0dfcaa021a89d0226ae6701193690c1a164e7cb6
Anush2303 <74965306+Anush2303@users.noreply.github.com>
2021-12-30 08:35:55 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../css/Navbar.css';
 import Nav from 'react-bootstrap/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faSearch,faShoppingCart,faUser} from '@fortawesome/free-solid-svg-icons';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 function Navbar(){
     return(
